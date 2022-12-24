964a2a7896ebbc15b49b8980e36379ee2cf30e33
Sagar Hiray <63414334+thesagarhiray@users.noreply.github.com>
2022-12-24 12:19:02 +0530

    Add files via upload

---
+++ Yes Bank Web Page/YesBankLogin.js
@@ -0,0 +1,13 @@
+function validate(a,b) {
+    var userID=9766583610;
+    var password="sagarhiray";
+    var userID1=a;
+    var password1=b;
+    if(userID==userID1 && password==password1){
+        window.location.assign("HDFCMSG.html")
+    }
+    else{
+        window.location.assign("HDFCMSG1.html")
+    }
+
+}
